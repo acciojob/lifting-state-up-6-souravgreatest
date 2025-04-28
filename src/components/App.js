7d9f57b5c34ab3214f4c80b8bb4e280aeba0a8f7
souravgreatest <jainsourav220@gmail.com>
2025-04-28 10:50:36 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import './../styles/App.css';
-import TodoList from "./TodoList";'./TodoList.js';
+import "./../styles/App.css";
+import TodoList from "./TodoList";
 
 const App = () => {
   const [todos,setTodos]=useState([
@@ -8,17 +8,29 @@ const App = () => {
     {text:"Build a React app",completed:false},
     {text:"Deploy the React app",completed:false}
   ]);
-  const handleComplete=(index)=>{
-    let newTodo=[...todos];
-    newTodo[index].completed=true;
-    setTodos(newTodo);
-  }
+
+  const handleComplete = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = true;
+    setTodos(updatedTodos); // Update the state
+  };
+
   return (
     <div>
-      <h1>Parent Component</h1>
+      <h3>Todo App</h3>
       <TodoList arr={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+
+/*
+const [todos,setTodos]=useState([
+  {text:"Learn React",completed:false},
+  {text:"Build a React app",completed:false},
+  {text:"Deploy the React app",completed:false}
+]);
+
+*/
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const TodoList = ({ arr, handleComplete }) => {
   return (
     <div>
       <h2>Child Component</h2>
-      {arr.map((item, index) => ( 
-        <div key={index}>
-          <span>{item.text}</span>
-          {!item.completed && (<button onClick={() => handleComplete(index)}>Complete</button>)}
-        </div>
-      ))}
+      <ul>
+        {arr.map((item, index) => (
+          <li key={index}>
+            <span
+              style={{
+                textDecoration: item.completed ? "line-through" : "none",
+              }}
+            >
+              {item.text}
+            </span>
+            {!item.completed && (
+              <button onClick={() => handleComplete(index)}>Complete</button>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default TodoList;
